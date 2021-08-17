c0690ba6a49fc8efb46b20f20b0d73388d048029
Souvik Sardar <35128994+Souvik12345678@users.noreply.github.com>
2021-08-17 17:31:51 +0530

    W

---
+++ Assets/ScriptableObjs/LevelObj.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Level Object")]
+public class LevelObj : ScriptableObject
+{
+    [SerializeField]
+    private GameObject level1Prefab;
+    [SerializeField]
+    private GameObject level2Prefab;
+    [SerializeField]
+    private GameObject level3Prefab;
+    [SerializeField]
+    private GameObject level4Prefab;
+    [SerializeField]
+    private GameObject level5Prefab;
+    [SerializeField]
+    private GameObject level6Prefab;
+
+    // This function is called when the script is started
+    private void Awake()
+    {
+
+    }
+
+
+
+}
